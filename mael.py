abe3c5aea124b4e93ae2b0e0d3e015d0d45e9bdf
Mael <72377688+ImMael@users.noreply.github.com>
2022-06-20 10:32:15 +0200

    Update mael.py

--- mael.py
+++ mael.py
@@ -1,11 +1,11 @@
 import sys
 
 while True:
-    year = int(input("Entrez une année : "))
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-        print("L'année", year, "est bissextile.")
+    annee = int(input("Entrez une année : "))
+    if annee % 4 == 0 and annee % 100 != 0 or annee % 400 == 0:
+        print("L'année", annee, "est bissextile.")
     else:
-        print("L'année", year, "n'est pas bissextile.")
+        print("L'année", annee, "n'est pas bissextile.")
     if input("Voulez-vous continuer ? (o/n) ") == "n":
         sys.exit()
     else:
